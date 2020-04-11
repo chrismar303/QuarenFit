13c30e07e1da4e105e3b25e4d7700ad86aa9a9b6
chrismar303 <chrismar303@gmail.com>
2020-04-11 12:18:16 -0700

    Changed calendar to standard CalendarView | Document detailing ideas has been Added (was not in last commit) | Added sample images to show current state of app

--- app/src/main/java/com/ctech/chris/quarenfit/StatsFragment.java
+++ app/src/main/java/com/ctech/chris/quarenfit/StatsFragment.java
@@ -7,42 +7,27 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
 import android.widget.Toast;
 
-import com.squareup.timessquare.CalendarPickerView;
-
-import java.text.DateFormat;
-import java.util.Calendar;
-import java.util.Date;
 
 public class StatsFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
         View layout = inflater.inflate(R.layout.fragment_stats, container, false);
         initCalendar(layout);
         return layout;
     }
 
     private void initCalendar(View layout) {
-        // setup calendar
-        Date today = new Date();
-        Calendar nextYear = Calendar.getInstance();
-        nextYear.add(Calendar.YEAR, 1);
-
-        CalendarPickerView datePicker = (CalendarPickerView) layout.findViewById(R.id.calendar);
-        datePicker.init(today, nextYear.getTime()).withSelectedDate(today); // set min and max dates
-
-        datePicker.setOnDateSelectedListener(new CalendarPickerView.OnDateSelectedListener() {
+        CalendarView calendarView = (CalendarView) layout.findViewById(R.id.calendar);
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
-            public void onDateSelected(Date date) {
-                String selectedDate = DateFormat.getDateInstance().format(date);
-                Toast.makeText(getActivity().getApplicationContext(), selectedDate, Toast.LENGTH_SHORT).show();
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+                String date = month + "/" + dayOfMonth + "/" + year;
+                Toast.makeText(getActivity().getApplicationContext(), date, Toast.LENGTH_SHORT).show();
             }
-
-            @Override
-            public void onDateUnselected(Date date) { }
         });
     }
 }
