db9fb24b3e1838eb334ea268f436b476e0527936
chrismar303 <chrismar303@gmail.com>
2020-04-15 00:12:05 -0700

    LogEntryActivity.java: Added functionality to radio groups to make list of workouts and new workout buttons disappear if "no" is selected. Reappears if "yes" is selected

--- app/src/main/java/com/ctech/chris/quarenfit/LogEntryActivity.java
+++ app/src/main/java/com/ctech/chris/quarenfit/LogEntryActivity.java
@@ -3,6 +3,9 @@ package com.ctech.chris.quarenfit;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,9 +26,63 @@ public class LogEntryActivity extends AppCompatActivity {
     //TODO: if no date is selected, date at top will be blank in log_entry
     // TODO: load any data into fields from db
     private void setupActivityUI() {
+        // set title as today's date
         TextView titleTV = (TextView) findViewById(R.id.entryTV);
         titleTV.setText(dateStr);
+
+        // setup radio groups functionality
+        initRadioGroups();
     }
+
+    private void initRadioGroups() {
+        initStrengthRGP(findViewById(R.id.strengthTrainingListContainer));
+        initCardioRGP(findViewById(R.id.cardioListContainer));
+    }
+
+    // TODO database
+    private void initStrengthRGP(final View strengthListContainer) {
+        RadioGroup strengthRGP = (RadioGroup) findViewById(R.id.strengthTrainingRGP);
+        strengthRGP.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch(checkedId)
+                {
+                    case R.id.yesStrengthTrainingRB:
+                        strengthListContainer.setVisibility(View.VISIBLE);
+                        break;
+                    case R.id.noStrengthTrainingRB:
+                        strengthListContainer.setVisibility(View.GONE);
+                        break;
+                }
+            }
+        });
+
+    }
+
+
+    private void initCardioRGP(final View cardioListContainer) {
+
+        RadioGroup cardioRGP = (RadioGroup) findViewById(R.id.cardioRGP);
+        cardioRGP.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch(checkedId)
+                {
+                    case R.id.yesCardioRB:
+                        cardioListContainer.setVisibility(View.VISIBLE);
+                        break;
+                    case R.id.noCardioRB:
+                        cardioListContainer.setVisibility(View.GONE);
+                        break;
+                }
+            }
+        });
+
+    }
+
+
+
+
 
 
     // TODO: close activity
